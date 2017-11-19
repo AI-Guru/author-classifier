8f596e2c48bb3a486f9a4cf286ca290121f8399b
Tristan Behrens <tristan.behrens@porschedigitallab.com>
2017-11-19 16:48:39 +0100

    Cleanup of analysis.

--- analyze_corpus.py
+++ analyze_corpus.py
@@ -11,26 +11,23 @@ from nltk.corpus import stopwords
 import string
 from collections import Counter
 
+# Always remove all analysis-data if true.
 remove_analysis = True
+
 
 print("Loading spacy-model...")
 spacy_german = spacy.load('de')
 german_stopwords = stopwords.words("german")
 punctuations = string.punctuation
 
-# TODO dokumentieren python -m spacy.de.download
-# Ansehen TODO https://explosion.ai/blog/german-model
-# TODO More output
 
 def main(args=None):
-
     corpus_path = "corpus"
     analysis_path = "analysis"
     analyze_corpus(corpus_path, analysis_path)
 
 
 def analyze_corpus(corpus_path="corpus", analysis_path="analysis"):
-
     print("Analyzing corpus...")
 
     # Make sure that there is a proper folder for analysis.
@@ -39,11 +36,11 @@ def analyze_corpus(corpus_path="corpus", analysis_path="analysis"):
     if not os.path.exists(analysis_path):
         os.makedirs(analysis_path)
 
-    # Get all authors from the corpus.
+    # Get all authors from the corpus and process them individually.
     authors = [element for element in os.listdir(corpus_path) if not os.path.isfile(os.path.join(corpus_path, element))]
-
     counts = []
     for author in authors:
+        # Process an author. Get full string, individual tokens and token count.
         all_text, all_tokens, all_count = process_author(corpus_path, author)
 
         # Render the word cloud.
@@ -56,15 +53,13 @@ def analyze_corpus(corpus_path="corpus", analysis_path="analysis"):
         most_frequent_words_path = os.path.join(analysis_path, most_frequent_words_name)
         render_most_frequent_words(author, all_tokens, most_frequent_words_path)
 
-        # TODO count words, files, or paragraphs
+        # Overall count.
         counts.append(all_count)
 
     # Render corpus distribution.
-    sns_plot = sns.barplot(x=authors, y=counts)
     corpus_distribution_name = "corpus_distribution.png"
     corpus_distribution_path = os.path.join(analysis_path, corpus_distribution_name)
-    sns_plot.get_figure().savefig(corpus_distribution_path)
-    print("Written corpus-distribution to", corpus_distribution_path)
+    render_corpus_distribution(authors, counts, corpus_distribution_path)
 
 
 def process_author(corpus_path, author):
@@ -72,6 +67,7 @@ def process_author(corpus_path, author):
     all_tokens = []
     all_count = 0
 
+    # Get all documents from author and get data.
     glob_path = os.path.join(corpus_path, author, "*.body.txt")
     body_file_paths = glob.glob(glob_path)
     for body_file_path in body_file_paths:
@@ -85,10 +81,14 @@ def process_author(corpus_path, author):
 
     return all_text, all_tokens, all_count
 
+
 def clean_up_text(text):
     doc = spacy_german(text)
 
+    # Go to lower-case.
     tokens = [tok.lemma_.lower().strip() for tok in doc if tok.lemma_ != '-PRON-']
+
+    # Remove stopwords.
     tokens = [tok for tok in tokens if tok not in german_stopwords and tok not in punctuations]
 
     text = ' '.join(tokens)
@@ -101,17 +101,12 @@ def render_word_cloud(all_text, word_cloud_path):
     figure = plt.figure(figsize=(15, 12))
     plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis('off')
-    #plt.show()
     figure.savefig(word_cloud_path)
     print("Written word-cloud to", word_cloud_path)
 
 
 def render_most_frequent_words(author, tokens, most_frequent_words_path):
-
-    # Clean up the text.
-    #clean_text = [word for word in clean_text if word != '\'s']
-
-    counts = Counter(tokens) # TODO long string or sequence?
+    counts = Counter(tokens)
 
     common_words = [word[0] for word in counts.most_common(25)]
     common_counts = [word[1] for word in counts.most_common(25)]
@@ -119,10 +114,14 @@ def render_most_frequent_words(author, tokens, most_frequent_words_path):
     figure = plt.figure(figsize=(15, 12))
     sns.barplot(x=common_words, y=common_counts)
     plt.title("Most Common Words used by " + author)
-    #plt.show()
-
     figure.savefig(most_frequent_words_path)
     print("Written most frequent words to", most_frequent_words_path)
+
+
+def render_corpus_distribution(authors, counts, corpus_distribution_path):
+    sns_plot = sns.barplot(x=authors, y=counts)
+    sns_plot.get_figure().savefig(corpus_distribution_path)
+    print("Written corpus-distribution to", corpus_distribution_path)
 
 
 if __name__ == "__main__":
